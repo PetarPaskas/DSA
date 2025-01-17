f9c8c356934ffbe1e45ac3a12f17372002df696c
Petar <petar.paskas@hotmail.com>
2025-01-17 22:19:03 +0100

    completed: contains_duplicate

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,13 @@
+﻿
+var nums = new int[]{1,2,3,4,4};
+Console.WriteLine("Has duplicate: " + hasDuplicate(nums));
+
+
+bool hasDuplicate(int[] nums){
+    var inputSet = new HashSet<int>();
+    for(int i = 0; i<nums.Length;i++){
+        if(!inputSet.Add(nums[i]))
+        return true;
+    }
+    return false;
+}
