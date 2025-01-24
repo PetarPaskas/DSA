8f78cc0848ae0029336055f5140564845ffcee23
Petar <petar.paskas@hotmail.com>
2025-01-24 16:15:44 +0100

    11. TwoSum2InputArrayIsSorted

---
+++ 11. TwoSum2InputArrayIsSorted/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//array of nums sorted in ascending
+foreach(var x in TwoSum([1,2,3,4],3))
+{
+    Console.WriteLine(x);
+}
+int[] TwoSum(int[] numbers, int target)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+
+        for (int j = i + 1; j < numbers.Length; j++) 
+        {
+
+            if ((numbers[i] + numbers[j]) == target)
+                return [i + 1, j + 1];
+        }
+
+    }
+
+    return [];
+}
