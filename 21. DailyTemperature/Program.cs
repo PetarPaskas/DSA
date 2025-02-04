f21c0ca663fa0a1456af8dad1ffe409003b43e7d
PetarPaskas <petar.paskas@hotmail.com>
2025-02-04 15:16:42 +0100

    Daily Temperature

---
+++ 21. DailyTemperature/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] DailyTemperatures(int[] temperatures)
+{
+    int[] result = new int[temperatures.Length];
+
+    for (int i = 0; i < temperatures.Length; i++)
+    {
+        var currentlyObserved = temperatures[i];
+
+        for (int j = i + 1; j < temperatures.Length; j++)
+        {
+            var day = temperatures[j];
+
+            if (day > currentlyObserved)
+            {
+                var daySince = j - i;
+                result[i] = daySince;
+                break;
+            }
+        }
+    }
+
+    return result;
+
+}
