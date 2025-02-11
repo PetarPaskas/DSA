1ddb596c3ef9de63c424c9ea849353f0af3de329
Petar <petar.paskas@hotmail.com>
2025-02-11 18:03:46 +0100

    LengthOfLongestSubstring

---
+++ 24. LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+string s = "au";
+Console.WriteLine(LengthOfLongestSubstring(s));
+int LengthOfLongestSubstring(string s)
+{
+    if (String.IsNullOrEmpty(s))
+        return 0;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        int itemsSinceStart = s.Length - i;
+        for (int j = 0; itemsSinceStart + j <= s.Length; j++)
+        {
+            int startIndex = j;
+
+            var subString = s.Substring(startIndex, itemsSinceStart);
+            HashSet<char> charHash = new HashSet<char>(subString);
+
+            if (subString.Length == charHash.Count)
+                return subString.Length;
+
+        }
+
+    }
+
+    return 1;
+
+
+}
