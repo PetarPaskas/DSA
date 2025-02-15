dd9e35e6d09f80db05e6df1c570bef13fc451123
Petar <petar.paskas@hotmail.com>
2025-02-15 14:07:08 +0100

    BinarySearch

---
+++ 26. BinarySearch/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//[-1,0,2,4,6,8]
+//target = 4
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        return Search(nums, target, 0, nums.Length - 1);
+    }
+
+    private int Search(int[] nums, int target, int left, int right)
+    {
+
+        if (left > right)
+            return -1;
+
+        int middle = (left + right) / 2;
+
+        if (nums[middle] == target)
+            return middle;
+
+        if (nums[middle] > target)
+            return Search(nums, target, left, middle - 1);
+
+        return Search(nums, target, middle + 1, right);
+    }
+}
