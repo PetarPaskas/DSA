b223dd73d631c74c83becdb729995a28212be1bb
Petar <petar.paskas@hotmail.com>
2025-02-19 22:21:11 +0100

    29. Reverese LinkedList

---
+++ 29. Reverse Linked List/Program.cs
@@ -0,0 +1,48 @@
+﻿ 
+
+
+
+public class ListNode {
+     public int val;
+     public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+         this.val = val;
+        this.next = next;
+    }
+  }
+
+public class Solution
+{
+    private Stack<int> _stack = new Stack<int>(); //Jako lose resenje, bukvalno sam zaboravio kako se radi sve
+
+    public ListNode ReverseList(ListNode head)
+    {
+
+        PopulateStack(head);
+
+        if (_stack.Count == 0)
+            return null;
+
+        ListNode resultNode = new ListNode(_stack.Pop());
+        var first = resultNode;
+
+        while (_stack.Count > 0)
+        {
+            var next = new ListNode(_stack.Pop());
+            resultNode.next = next;
+            resultNode = next;
+        }
+
+        return first;
+    }
+
+    private void PopulateStack(ListNode head)
+    {
+        if (head == null)
+            return;
+
+        _stack.Push(head.val);
+
+        PopulateStack(head.next);
+    }
+}
