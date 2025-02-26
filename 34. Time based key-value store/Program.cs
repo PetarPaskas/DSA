d3d999632b9981e6292ca99d174e316a742ce39f
Petar <petar.paskas@hotmail.com>
2025-02-26 13:32:41 +0100

    Time based key-value store

---
+++ 34. Time based key-value store/Program.cs
@@ -0,0 +1,50 @@
+﻿public class TimeMap
+{
+
+    public Dictionary<string, List<KVMap>> _items = new Dictionary<string, List<KVMap>>();
+
+    public TimeMap()
+    {
+
+    }
+
+    public void Set(string key, string value, int timestamp)
+    {
+        var map = new KVMap(key, value, timestamp);
+        if (!_items.ContainsKey(key))
+            _items.Add(key, new List<KVMap>());
+
+        _items[key].Add(map);
+
+    }
+
+    public string Get(string key, int timestamp)
+    {
+        if (!_items.ContainsKey(key))
+            return string.Empty;
+
+        int seen = -1;
+
+        for (int i = 0; i < _items[key].Count; i++) {
+            if (_items[key][i].Timestamp <= timestamp)
+                seen = i;
+        }
+
+        return seen == -1 ? string.Empty : _items[key][seen].Value;
+    }
+
+}
+
+public class KVMap
+{
+    public string Key { get; set; }
+    public string Value { get; set; }
+    public int Timestamp { get; set; }
+
+    public KVMap(string key, string value, int timestamp)
+    {
+        Key = key;
+        Value = value;
+        Timestamp = timestamp;
+    }
+}
